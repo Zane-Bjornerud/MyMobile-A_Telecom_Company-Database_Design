8a2a2f14834ea3f0cbdea8408c81ccf1134930fc
Zane Bjornerud <122947902+Zane-Bjornerud@users.noreply.github.com>
2023-08-01 18:25:58 -0400

    Update Insert_Customer_Info.sql

--- SQL/Insert_Customer_Info.sql
+++ SQL/Insert_Customer_Info.sql
@@ -1,1 +1,6 @@
+INSERT INTO customer (customerID, customerName, streetAddress, phoneNumber, emailAddress, DateOfBirth)
+VALUES (0001, 'Lily Zhang', '123 Main St, Apt 2B', '(555) 555-1212', 'lilyzhang@example.com', '1995-03-16'),
+    (0002, 'Malik Johnson', '456 Oak Ave', '(555) 555-2323', 'malikjohnson@example.com', '1988-11-02'),
+    (0003, 'Sofia Patel', '789 Elm St, Suite 4', '(555) 555-3434', 'sofiapatel@example.com', '1992-07-12'),
+    (0004, 'Ethan Kim', '1010 Maple Dr', '(555) 555-4545', 'ethankim@example.com', '1999-04-29');
 
